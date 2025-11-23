56fa63e3ca12c288fe694f24da597290ba30214b
eunjung <jjonjunggame@gmail.com>
2025-11-23 17:53:59 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(int x) {
+    int n = x;
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return (x % sum) == 0;
+}
+
