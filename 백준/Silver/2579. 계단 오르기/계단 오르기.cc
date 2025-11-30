57a49f97fda8905da9eb71fefdf5556cd0d2de68
eunjung <jjonjunggame@gmail.com>
2025-12-01 08:25:29 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int stair[301];
+    for (int i = 1; i <= n; i++) {
+        cin >> stair[i];
+    }
+
+    int dp[301] = {0};
+
+    dp[1] = stair[1];
+    if (n >= 2) {
+        dp[2] = stair[1] + stair[2];
+    }
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = max(
+            dp[i - 2] + stair[i],                
+            dp[i - 3] + stair[i - 1] + stair[i] 
+        );
+    }
+
+    cout << dp[n] << "\n";
+    return 0;
+}
