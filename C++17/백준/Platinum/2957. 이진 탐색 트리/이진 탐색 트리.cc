90db23e9b5798e22eee61c0dcaf52359925b313f
eunjung <jjonjunggame@gmail.com>
2025-12-06 19:58:59 +0900

    [Platinum V] Title: 이진 탐색 트리, Time: 152 ms, Memory: 18492 KB -BaekjoonHub

--- C++17/백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.cc
+++ C++17/백준/Platinum/2957. 이진 탐색 트리/이진 탐색 트리.cc
@@ -1,42 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N;
-    if (!(cin >> N)) return 0;
-    vector<int> a(N);
-    for (int i = 0; i < N; ++i) cin >> a[i];
-
-    set<int> s;                    
-    vector<int> depth(N + 1, -1);   
-    long long C = 0;
-
-    s.insert(a[0]);
-    depth[a[0]] = 0;
-    cout << 0 << '\n';
-
-    for (int i = 1; i < N; ++i) {
-        int x = a[i];
-        auto it = s.lower_bound(x);
-        int dl = -1; 
-        int dr = -1;
-        if (it != s.end()) {
-           
-            dr = depth[*it];
-        }
-        if (it != s.begin()) {
-            auto it2 = it;
-            --it2;
-            dl = depth[*it2];
-        }
-        int d = max(dl, dr) + 1; 
-        depth[x] = d;
-        s.insert(x);
-        C += d;              
-        cout << C << '\n';
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    
+    int N; if(!(cin>>N)) return 0;
+    
+    vector<int>a(N); for(int i=0;i<N;i++) cin>>a[i];
+    
+    set<int>s; vector<int>d(N+1,-1); long long C=0;
+    
+    s.insert(a[0]); d[a[0]]=0; cout<<0<<"\n";
+    
+    for(int i=1;i<N;i++){
+        int x=a[i]; auto it=s.lower_bound(x);
+        int L=-1,R=-1;
+        if(it!=s.end()) R=d[*it];
+        if(it!=s.begin()){ --it; L=d[*it]; }
+        d[x]=max(L,R)+1; s.insert(x); C+=d[x];
+        cout<<C<<"\n";
     }
-    return 0;
+    
 }
